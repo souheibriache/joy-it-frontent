03c50db722c3dba1f35fbfd7e39f7c0d1da0fbd8
s.riache <s.riache@cyberdian.fr>
2025-01-26 15:34:13 +0100

    refactor: remove unused variables

--- src/components/AccountVerification.tsx
+++ src/components/AccountVerification.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { useVerifyAccount } from "@/utils/api/user-api";
